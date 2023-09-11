03001c5cd65770cb8963d40f1d1cb646d659f656
adminksa-com <adminksa-com@infinigate.com>
2023-09-11 08:02:40 +0000

    Merged PR 2895: Added OSS to Bronze
    Added OSS to Bronze

--- databricks/bronze/nb-cloudblue-register-bronze.py
+++ databricks/bronze/nb-cloudblue-register-bronze.py
@@ -35,3 +35,30 @@ for tableName in dbutils.fs.ls('mnt/bronze/cloudblue/pba'):
     print(f"STARTING: Registering table '{tableName.name[:-1]}' to catalog 'bronze_{ENVIRONMENT}' in schema 'cloudblue_pba'.")
     registerTable(tableName.name[:-1])
     print(f"FINISHED: Registering table '{tableName.name[:-1]}' to catalog 'bronze_{ENVIRONMENT}' in schema 'cloudblue_pba'.")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use schema cloudblue_oss;
+
+# COMMAND ----------
+
+def registerTable(tableName : str, recreate : bool = False):
+    cleanedTableName = tableName.replace(" ", "")
+    if recreate:
+        spark.sql(f"""
+                  DROP TABLE IF EXISTS {cleanedTableName}
+                  """)
+    if not spark.catalog.tableExists(cleanedTableName):
+        spark.sql(f"""
+        Create table {cleanedTableName}
+        using parquet
+        Location 'abfss://bronze@adls0ig0{ENVIRONMENT}0westeurope.dfs.core.windows.net/cloudblue/oss/{tableName}/'
+        """)
+
+# COMMAND ----------
+
+for tableName in dbutils.fs.ls('mnt/bronze/cloudblue/oss'):
+    print(f"STARTING: Registering table '{tableName.name[:-1]}' to catalog 'bronze_{ENVIRONMENT}' in schema 'cloudblue_oss'.")
+    registerTable(tableName.name[:-1])
+    print(f"FINISHED: Registering table '{tableName.name[:-1]}' to catalog 'bronze_{ENVIRONMENT}' in schema 'cloudblue_oss'.")
