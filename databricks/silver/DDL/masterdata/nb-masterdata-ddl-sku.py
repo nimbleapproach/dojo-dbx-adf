6cfb4812e5aba3f5e46532fe768c23f4a1ce4a13
adminksa-com <adminksa-com@infinigate.com>
2023-10-05 13:01:35 +0000

    Merged PR 3176: hf
    hf

--- databricks/silver/DDL/masterdata/nb-masterdata-ddl-sku.py
+++ databricks/silver/DDL/masterdata/nb-masterdata-ddl-sku.py
@@ -44,7 +44,7 @@ spark.catalog.setCurrentCatalog(f"silver_{ENVIRONMENT}")
 # MAGIC       COMMENT 'HashKey over all but Sys columns.'
 # MAGIC     ,Sys_Silver_IsCurrent BOOLEAN
 # MAGIC       COMMENT 'Flag if this is the current version.'
-# MAGIC ,CONSTRAINT SKU_pk PRIMARY KEY(SKU)
+# MAGIC ,CONSTRAINT SKU_pk PRIMARY KEY(SKU,Sys_Bronze_InsertDateTime_UTC)
 # MAGIC   )
 # MAGIC COMMENT 'This table contains the line data for account. \n' 
 # MAGIC TBLPROPERTIES ('delta.feature.allowColumnDefaults' = 'supported')
