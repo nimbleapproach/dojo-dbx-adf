d8a19dc57146da208cbce222de647216ee40e3cf
adminksa-com@infinigate.com <adminksa-com@infinigate.com>
2023-10-27 11:33:44 +0000

    DDL dati_saldi_lordi update

--- databricks/silver/DDL/tag02/nb-tag02-silver-ddl-dati-saldi-lordi.sql
+++ databricks/silver/DDL/tag02/nb-tag02-silver-ddl-dati-saldi-lordi.sql
@@ -31,31 +31,31 @@ CREATE OR REPLACE TABLE dati_saldi_lordi
   ( SID bigint
     GENERATED ALWAYS AS IDENTITY
     ,OID_DATI_SALDI_LORDI STRING
-      COMMENT 'todo'
+      COMMENT 'OID'
     ,COD_SCENARIO     STRING
-      COMMENT 'todo'
+      COMMENT 'This is an active original scenario'
     ,COD_PERIODO    STRING
-      COMMENT 'todo'
+      COMMENT 'This is a period belonging to the Scenario.'
     ,COD_AZIENDA    STRING
-      COMMENT 'todo'
+      COMMENT 'Entity code'
     ,COD_CONTO    STRING
-      COMMENT 'todo'
+      COMMENT 'Account code.'
     ,COD_DEST1    STRING
-      COMMENT 'todo'
+      COMMENT 'Custom Dimension'
     ,COD_DEST2    STRING
-      COMMENT 'todo'
+      COMMENT 'Custom Dimension'
     ,COD_DEST3    STRING
-      COMMENT 'todo'
+      COMMENT 'Custom Dimension'
     ,COD_DEST4    STRING
-      COMMENT 'todo'
+      COMMENT 'Custom Dimension'
     ,COD_CATEGORIA    STRING
-      COMMENT 'todo'
+      COMMENT 'This is an "Amount" category.'
     ,IMPORTO    DECIMAL(20,2)
-      COMMENT 'todo'
+      COMMENT 'Amount in entity curreny.'
     ,COD_VALUTA   STRING
-      COMMENT 'todo'
+      COMMENT 'Entity currency code.'
     ,PROVENIENZA    STRING
-      COMMENT 'todo'
+      COMMENT 'TODO'
 
     ,DATEUPD TIMESTAMP
       COMMENT 'Last Update Timestamp'
@@ -70,11 +70,11 @@ CREATE OR REPLACE TABLE dati_saldi_lordi
     ,Sys_Silver_HashKey BIGINT NOT NULL
       COMMENT 'HashKey over all but Sys and DATEUPD columns.'
       ,Sys_Silver_IsCurrent BOOLEAN
-,CONSTRAINT dati_saldi_lordi_pk PRIMARY KEY(COD_PERIODO,COD_SCENARIO,COD_AZIENDA,COD_CONTO,COD_DEST1,COD_DEST2,COD_DEST3,COD_DEST4,PROVENIENZA,COD_CATEGORIA ,DATEUPD)
+,CONSTRAINT dati_saldi_lordi_pk PRIMARY KEY(COD_PERIODO,COD_SCENARIO,OID_DATI_SALDI_LORDI,DATEUPD)
   )
 COMMENT 'This table contains the "fact information".'
 TBLPROPERTIES ('delta.feature.allowColumnDefaults' = 'supported')
-CLUSTER BY (COD_PERIODO,COD_SCENARIO,COD_AZIENDA)
+CLUSTER BY (COD_PERIODO,COD_SCENARIO)
 
 -- COMMAND ----------
 
