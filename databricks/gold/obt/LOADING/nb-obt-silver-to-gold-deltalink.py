c01fa02a54a6e43619bc19fc8c0a175b1eb8bd3b
Ying-Zhen Chen <ying-zhen.chen@infinigate.com>
2023-11-27 09:49:17 +0000

    Merged PR 3723: deltalink bronze-silver-gold
    - ddl
    - silver to gold

---
+++ databricks/gold/obt/LOADING/nb-obt-silver-to-gold-deltalink.py
@@ -0,0 +1,129 @@
+# Databricks notebook source
+import os
+
+ENVIRONMENT = os.environ["__ENVIRONMENT__"]
+
+# COMMAND ----------
+
+spark.catalog.setCurrentCatalog(f"gold_{ENVIRONMENT}")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC Use SCHEMA obt
+
+# COMMAND ----------
+
+# DBTITLE 1,Silver to Gold Deltalink
+spark.sql(
+    f"""         
+CREATE OR REPLACE VIEW deltalink_globaltransactions AS
+
+with cte as 
+(select 
+
+'NU' AS GroupEntityCode,
+'BE3' AS EntityCode,
+invoice.InvoiceNumber,
+TO_DATE(CAST(UNIX_TIMESTAMP(invoice.InvoiceDate, 'dd/MM/yyyy') AS timestamp)) as TransactionDate,
+TO_DATE('1900-01-01' ) AS SalesOrderDate,
+RIGHT(invoice.OrderNumber,9) AS SalesOrderID,
+"NaN" AS SalesOrderItemID,
+COALESCE(Artcode, "NaN") AS SKUInternal,
+COALESCE(datanowarr.SKU, 'NaN')  AS SKUMaster,
+COALESCE(Description,'NaN') AS Description,
+COALESCE(ArticleProductType,'NaN') AS ProductTypeInternal,
+COALESCE(datanowarr.Product_Type,'NaN') AS ProductTypeMaster,
+coalesce(datanowarr.Commitment_Duration_in_months, 'NaN') AS CommitmentDuration1Master,
+coalesce(datanowarr.Commitment_Duration_Value, 'NaN') AS CommitmentDuration2Master,
+coalesce(datanowarr.Billing_Frequency, 'NaN') AS BillingFrequencyMaster,
+coalesce(datanowarr.Consumption_Model, 'NaN') AS ConsumptionModelMaster,
+coalesce(SupplierID,   'NaN') AS VendorCode,
+coalesce(Supplier, 'NaN') AS VendorNameInternal,
+coalesce(datanowarr.Vendor_Name, 'NaN') AS VendorNameMaster,
+'BE3' AS VendorGeography,
+to_date('1900-01-01') AS VendorStartDate,
+coalesce(ClientNumber,'NaN') AS ResellerCode,
+coalesce(Company,'NaN') AS ResellerNameInternal,
+'BE3' AS ResellerGeographyInternal,
+to_date( '1900-01-01' ) AS ResellerStartDate,
+'NaN' AS ResellerGroupCode,
+'NaN' AS ResellerGroupName,
+to_date('1900-01-01' ) AS ResellerGroupStartDate,
+Currency AS CurrencyCode,
+cast(RevenueTransaction as DECIMAL(10, 2)) AS RevenueAmount
+ from silver_{ENVIRONMENT}.deltalink.invoicedata as invoice
+LEFT JOIN gold_{ENVIRONMENT}.obt.datanowarr AS datanowarr ON datanowarr.SKU = invoice.ArtSupCode
+where invoice.Sys_Silver_IsCurrent = 1)
+
+  select
+  GroupEntityCode,
+  EntityCode,
+  TransactionDate,
+  SalesOrderDate,
+  SalesOrderID,
+  SalesOrderItemID,
+  SKUInternal,
+  SKUMaster,
+  Description,
+  ProductTypeInternal,
+  ProductTypeMaster,
+  CommitmentDuration1Master,
+  CommitmentDuration2Master,
+  BillingFrequencyMaster,
+  ConsumptionModelMaster,
+  VendorCode,
+  VendorNameInternal,
+  VendorNameMaster,
+  EntityCode as VendorGeography,
+  case
+    when VendorStartDate <= '1900-01-01' then min(TransactionDate) OVER(PARTITION BY EntityCode, VendorCode)
+    else VendorStartDate
+  end as VendorStartDate,
+  substring_index(ResellerCode, ' ', 1)ResellerCode,
+  ResellerNameInternal,
+  ResellerGeographyInternal,
+  case
+    when ResellerStartDate <= '1900-01-01' then min(TransactionDate) OVER(PARTITION BY EntityCode, ResellerCode)
+    else ResellerStartDate
+  end as ResellerStartDate,
+  ResellerGroupCode,
+  ResellerGroupName,
+case
+    when ResellerStartDate <= '1900-01-01' then min(TransactionDate) OVER(PARTITION BY EntityCode, ResellerCode)
+    else ResellerStartDate
+  end as ResellerGroupStartDate,
+  CurrencyCode,
+  RevenueAmount
+from
+  cte""")
+
+# COMMAND ----------
+
+df_obt = spark.read.table("globaltransactions")
+
+df_sl = spark.read.table(f"gold_{ENVIRONMENT}.obt.deltalink_globaltransactions")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+
+target_columns = df_obt.columns
+
+source_columns = df_sl.columns
+
+intersection_columns = [column for column in target_columns if column in source_columns]
+
+selection_columns = [
+    col(column) for column in intersection_columns if column not in ["SID"]
+]
+
+# COMMAND ----------
+
+df_selection = df_sl.select(selection_columns)
+df_selection = df_selection.fillna(value= 'NaN').replace('', 'NaN')
+
+# COMMAND ----------
+
+df_selection.write.mode("overwrite").option("replaceWhere", "GroupEntityCode = 'NU' AND EntityCode = 'BE3'").saveAsTable("globaltransactions")
---
+++ databricks/silver/DDL/deltalink/nb-deltalink-silver-ddl-invoicedata.sql
@@ -0,0 +1,85 @@
+-- Databricks notebook source
+-- MAGIC %python
+-- MAGIC import os
+-- MAGIC
+-- MAGIC ENVIRONMENT = os.environ["__ENVIRONMENT__"]
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC The target catalog depens on the enivronment. Since we are using Unity Catalog we need to use a unqiue name for the catalog. This is the reason why we name the dev silver catalog "silver_dev" for example.
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC spark.catalog.setCurrentCatalog(f"silver_{ENVIRONMENT}")
+
+-- COMMAND ----------
+
+USE SCHEMA deltalink;
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TABLE invoicedata
+  ( 
+    SID bigint
+        GENERATED ALWAYS AS IDENTITY
+        COMMENT 'Surrogate Key'
+    ,InvoiceNumber STRING
+        COMMENT 'TODO'  
+    ,LineNo   int
+      COMMENT 'TODO'
+    ,InvoiceDate STRING
+        COMMENT 'TODO'   
+    ,OrderType STRING
+        COMMENT 'TODO'   
+    ,OrderNumber int
+        COMMENT 'TODO'   
+    ,ClientNumber int
+        COMMENT 'TODO'   
+    ,Company STRING
+        COMMENT 'TODO'   
+    ,Artcode STRING
+        COMMENT 'TODO'   
+    ,SupplierID STRING
+        COMMENT 'TODO'   
+    ,Supplier STRING
+        COMMENT 'TODO'   
+    ,ArtSupCode STRING
+        COMMENT 'TODO'   
+    ,Description STRING
+        COMMENT 'TODO'   
+    ,Quantity FLOAT
+        COMMENT 'TODO'   
+    ,ArticleProductType STRING
+        COMMENT 'TODO'   
+    ,Currency STRING
+        COMMENT 'TODO'   
+    ,RevenueTransaction DECIMAL(10,2)
+        COMMENT 'TODO'   
+    ,CostTransaction DECIMAL(10,2)
+        COMMENT 'TODO'   
+    ,MarginTransaction DECIMAL(10,2)
+        COMMENT 'TODO'
+    ,Sys_Bronze_InsertDateTime_UTC TIMESTAMP
+      COMMENT 'The timestamp when this entry landed in bronze.'
+    ,Sys_Silver_InsertDateTime_UTC TIMESTAMP
+      DEFAULT current_timestamp()
+      COMMENT 'The timestamp when this entry landed in silver.'
+    ,Sys_Silver_ModifedDateTime_UTC TIMESTAMP
+      DEFAULT current_timestamp()
+      COMMENT 'The timestamp when this entry was last modifed in silver.'
+    ,Sys_Silver_HashKey BIGINT NOT NULL
+      COMMENT 'HashKey over all but Sys columns.'
+    ,Sys_Silver_IsCurrent BOOLEAN
+,CONSTRAINT deltalink_invoicedata_pk PRIMARY KEY(InvoiceNumber,LineNo)
+  )
+COMMENT 'This table contains the line data for gl_entry. \n' 
+TBLPROPERTIES ('delta.feature.allowColumnDefaults' = 'supported')
+CLUSTER BY (InvoiceNumber,LineNo)
+
+-- COMMAND ----------
+
+ALTER TABLE invoicedata ADD CONSTRAINT dateWithinRange_Bronze_InsertDateTime CHECK (Sys_Bronze_InsertDateTime_UTC > '1900-01-01');
+ALTER TABLE invoicedata ADD CONSTRAINT dateWithinRange_Silver_InsertDateTime CHECK (Sys_Silver_InsertDateTime_UTC > '1900-01-01');
+ALTER TABLE invoicedata ADD CONSTRAINT dateWithinRange_Silver_ModifedDateTime CHECK (Sys_Silver_ModifedDateTime_UTC > '1900-01-01');
