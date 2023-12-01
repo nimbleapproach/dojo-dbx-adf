edffcd22fb80bf96f4f0790186e00c05ba88a2b0
Ying-Zhen Chen <ying-zhen.chen@infinigate.com>
2023-12-01 11:50:41 +0000

    Merged PR 3769: silver-gold-cloud_distribution

---
+++ databricks/gold/obt/LOADING/nb-obt-silver-to-gold-cloud_distribution.py
@@ -0,0 +1,140 @@
+# Databricks notebook source
+import os
+
+ENVIRONMENT = os.environ["__ENVIRONMENT__"]
+
+# COMMAND ----------
+
+spark.catalog.setCurrentCatalog(f"gold_{ENVIRONMENT}")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC Use SCHEMA obt
+
+# COMMAND ----------
+
+# DBTITLE 1,Silver to Gold Cloud Distribution
+spark.sql(
+    f"""         
+CREATE OR REPLACE VIEW netsafe_globaltransactions AS
+
+WITH initial_query AS (
+  SELECT
+    'NU' AS GroupEntityCode,
+    'UK4' AS EntityCode,
+    coalesce(
+      to_date(
+        CAST(
+          UNIX_TIMESTAMP(invoice.InvoiceDate, 'dd/MM/yyyy') AS timestamp
+        )
+      ),
+      to_date(invoice.InvoiceDate)
+    ) as TransactionDate,
+    coalesce(
+      to_date(
+        CAST(
+          UNIX_TIMESTAMP(invoice.InvoiceDate, 'dd/MM/yyyy') AS timestamp
+        )
+      ),
+      to_date(invoice.InvoiceDate)
+    ) as SalesOrderDate,
+    coalesce(invoice.InvoiceNumber, 'NaN') AS SalesOrderID,
+    coalesce(invoice.ItemID, 'NaN') AS SalesOrderItemID,
+    COALESCE(invoice.SKU, "NaN") AS SKUInternal,
+    COALESCE(datanowarr.SKU, 'NaN') AS SKUMaster,
+    COALESCE(invoice.SKUDescription, 'NaN') AS Description,
+    COALESCE(invoice.ItemType, 'NaN') AS ProductTypeInternal,
+    COALESCE(datanowarr.Product_Type, 'NaN') AS ProductTypeMaster,
+    coalesce(datanowarr.Commitment_Duration_in_months, 'NaN') AS CommitmentDuration1Master,
+    coalesce(datanowarr.Commitment_Duration_Value, 'NaN') AS CommitmentDuration2Master,
+    coalesce(datanowarr.Billing_Frequency, 'NaN') AS BillingFrequencyMaster,
+    coalesce(datanowarr.Consumption_Model, 'NaN') AS ConsumptionModelMaster,
+    coalesce(invoice.VendorID, 'NaN') AS VendorCode,
+    coalesce(invoice.VendorName, 'NaN') AS VendorNameInternal,
+    coalesce(datanowarr.Vendor_Name, 'NaN') AS VendorNameMaster,
+    'NaN' AS VendorGeography,
+    to_date('1900-01-01') AS VendorStartDate,
+    coalesce(invoice.CustomerAccount, 'NaN') AS ResellerCode,
+    coalesce(invoice.CustomerName, 'NaN') AS ResellerNameInternal,
+    'NaN' AS ResellerGeographyInternal,
+    to_date('1900-01-01') AS ResellerStartDate,
+    'NaN' AS ResellerGroupCode,
+    'NaN' AS ResellerGroupName,
+    to_date('1900-01-01') AS ResellerGroupStartDate,
+    Currency AS CurrencyCode,
+    cast(RevenueLocal as DECIMAL(10, 2)) AS RevenueAmount,
+    cast(CostLocal as DECIMAL(10, 2)) AS CostAmount,
+    cast(MarginLocal as DECIMAL(10, 2)) AS GP1
+  from
+    silver_{ENVIRONMENT}.cloud_distribution.invoicedata as invoice
+    LEFT JOIN gold_{ENVIRONMENT}.obt.datanowarr AS datanowarr ON datanowarr.SKU = invoice.SKU
+  WHERE
+    invoice.Sys_Silver_IsCurrent = true
+    and  invoice.MarginLocal is not null
+)
+SELECT
+  GroupEntityCode,
+  EntityCode,
+  TransactionDate,
+  SalesOrderDate,
+  SalesOrderID,
+  SalesOrderItemID,
+  SKUInternal,
+  SKUMaster,
+  Description,
+  ProductTypeInternal,
+  ProductTypeMaster,
+  CommitmentDuration1Master,
+  CommitmentDuration2Master,
+  BillingFrequencyMaster,
+  ConsumptionModelMaster,
+  VendorCode,
+  VendorNameInternal,
+  VendorNameMaster,
+  EntityCode as VendorGeography,
+  case
+    when VendorStartDate <= '1900-01-01' then min(TransactionDate) OVER(PARTITION BY EntityCode, VendorCode)
+    else VendorStartDate
+  end as VendorStartDate,
+  substring_index(ResellerCode, ' ', 1) ResellerCode,
+  ResellerNameInternal,
+  ResellerGeographyInternal,
+  case
+    when ResellerStartDate <= '1900-01-01' then min(TransactionDate) OVER(PARTITION BY EntityCode, ResellerCode)
+    else ResellerStartDate
+  end as ResellerStartDate,
+  ResellerGroupCode,
+  ResellerGroupName,
+  case
+    when ResellerStartDate <= '1900-01-01' then min(TransactionDate) OVER(PARTITION BY EntityCode, ResellerCode)
+    else ResellerStartDate
+  end as ResellerGroupStartDate,
+  CurrencyCode,
+  RevenueAmount,
+  CostAmount,
+  GP1
+from
+  initial_query""")
+
+# COMMAND ----------
+
+df_obt = spark.read.table("globaltransactions")
+df_netsafe = spark.read.table(f"gold_{ENVIRONMENT}.obt.netsafe_globaltransactions")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+target_columns = df_obt.columns
+source_columns = df_netsafe.columns
+intersection_columns = [column for column in target_columns if column in source_columns]
+selection_columns = [col(column) for column in intersection_columns if column not in ['SID']]
+
+# COMMAND ----------
+
+df_selection = df_netsafe.select(selection_columns)
+
+# COMMAND ----------
+
+df_selection.write.mode("overwrite").option("replaceWhere", "GroupEntityCode = 'NU' AND EntityCode ='UK4'").saveAsTable("globaltransactions")
---
+++ databricks/silver/DDL/cloud_distribution/nb-cloud_distribution-ddl-invoicedata.py
@@ -0,0 +1,105 @@
+# Databricks notebook source
+# MAGIC %python
+# MAGIC import os
+# MAGIC
+# MAGIC ENVIRONMENT = os.environ["__ENVIRONMENT__"]
+
+# COMMAND ----------
+
+spark.catalog.setCurrentCatalog(f"silver_{ENVIRONMENT}")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC USE SCHEMA cloud_distribution;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC CREATE OR REPLACE TABLE invoicedata
+# MAGIC   ( 
+# MAGIC     SID bigint
+# MAGIC       GENERATED ALWAYS AS IDENTITY
+# MAGIC       COMMENT 'Surrogate Key'
+# MAGIC     ,InvoiceNumber STRING
+# MAGIC       COMMENT 'TODO'  
+# MAGIC     ,InvoiceDate STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,SalesOrder STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,OrderType STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,ItemID STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,SKU STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,ItemType STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,CustomerName STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,Qty INT
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,Currency STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,RevenueLocal DECIMAL(38,18)
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,CostLocal DECIMAL(38,18)
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,MarginLocal DECIMAL(38,18)
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,AdjustmentLocal DECIMAL(38,18)
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,ExchangeRate FLOAT
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,RevenueGBP DECIMAL(38,18)
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,CostGBP DECIMAL(38,18)
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,MarginGBP DECIMAL(38,18)
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,VendorID STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,VendorName STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,AccountManager STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,QuoteID STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,CustomerAccount STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,SKUDescription STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,Entity STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,Sys_FileName STRING
+# MAGIC       COMMENT 'TODO'
+# MAGIC     ,Sys_ID STRING
+# MAGIC       COMMENT 'System Identifier for line records'
+# MAGIC     ,Sys_Bronze_InsertDateTime_UTC TIMESTAMP
+# MAGIC       COMMENT 'The timestamp when this entry landed in bronze.'
+# MAGIC     ,Sys_Silver_InsertDateTime_UTC TIMESTAMP
+# MAGIC       DEFAULT current_timestamp()
+# MAGIC       COMMENT 'The timestamp when this entry landed in silver.'
+# MAGIC     ,Sys_Silver_ModifedDateTime_UTC TIMESTAMP
+# MAGIC       DEFAULT current_timestamp()
+# MAGIC       COMMENT 'The timestamp when this entry was last modifed in silver.'
+# MAGIC     ,Sys_Silver_HashKey BIGINT NOT NULL
+# MAGIC       COMMENT 'HashKey over all but Sys columns.'
+# MAGIC     ,Sys_Silver_IsCurrent BOOLEAN
+# MAGIC       COMMENT 'Flag if this is the current version.'
+# MAGIC     ,Sys_Silver_IsDeleted BOOLEAN
+# MAGIC       COMMENT 'Flag if this is the deleted version.'
+# MAGIC ,CONSTRAINT invoicedata_pk PRIMARY KEY(InvoiceNumber, Sys_ID, Sys_Bronze_InsertDateTime_UTC)
+# MAGIC   )
+# MAGIC COMMENT 'This table contains the line data for ardoc. \n' 
+# MAGIC TBLPROPERTIES ('delta.feature.allowColumnDefaults' = 'supported')
+# MAGIC CLUSTER BY (InvoiceNumber, Sys_ID)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC ALTER TABLE invoicedata ADD CONSTRAINT dateWithinRange_Bronze_InsertDateTime CHECK (Sys_Bronze_InsertDateTime_UTC > '1900-01-01');
+# MAGIC ALTER TABLE invoicedata ADD CONSTRAINT dateWithinRange_Silver_InsertDateTime CHECK (Sys_Silver_InsertDateTime_UTC > '1900-01-01');
+# MAGIC ALTER TABLE invoicedata ADD CONSTRAINT dateWithinRange_Silver_ModifedDateTime CHECK (Sys_Silver_ModifedDateTime_UTC > '1900-01-01');
