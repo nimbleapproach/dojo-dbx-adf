cddf21920d959ba8dcab09ba32bf8ea2391ea2d5
adminksa-com <kevin.sames@drjve.de>
2024-01-25 12:20:32 +0000

    Merged PR 4018: Added Bronze NB NUAV SQLBYOD
    Added Bronze NB NUAV SQLBYOD

---
+++ databricks/bronze/nb-register-bronze-nuav-sqlbyod.py
@@ -0,0 +1,72 @@
+# Databricks notebook source
+import os
+
+ENVIRONMENT = os.environ["__ENVIRONMENT__"]
+
+# COMMAND ----------
+
+try:
+    REREGISTER = bool(dbutils.widgets.get("wg_reregister") == 'true')
+except:
+    dbutils.widgets.dropdown(name = "wg_reregister", defaultValue = 'false', choices =  ['false','true'])
+    REREGISTER = bool(dbutils.widgets.get("wg_reregister") == 'true')
+
+# COMMAND ----------
+
+try:
+    BRONZE_SCHEMA = dbutils.widgets.get("wg_bronzeSchema")
+except:
+    dbutils.widgets.text(name = "wg_bronzeSchema", defaultValue = 'igsql03')
+    BRONZE_SCHEMA = dbutils.widgets.get("wg_bronzeSchema")
+
+# COMMAND ----------
+
+try:
+    ROOT_PATH = dbutils.widgets.get("wg_rootPath")
+except:
+    dbutils.widgets.text(name = "wg_rootPath", defaultValue = 'igsql03')
+    ROOT_PATH = dbutils.widgets.get("wg_rootPath")
+
+# COMMAND ----------
+
+try:
+    TABLE_FORMAT = dbutils.widgets.get("wg_tableFormat")
+except:
+    dbutils.widgets.dropdown(name = "wg_tableFormat", defaultValue = 'parquet', choices =  ['parquet','delta'])
+    TABLE_FORMAT = dbutils.widgets.get("wg_tableFormat")
+
+# COMMAND ----------
+
+spark.catalog.setCurrentCatalog(f"bronze_{ENVIRONMENT}")
+
+# COMMAND ----------
+
+def registerTable(rootPath : str, tableName : str,schemaName : str,databaseName: str, recreate : bool = False):
+    tableName_uc = f"{schemaName}_{tableName}"
+    
+    spark.sql(f"""
+            use schema {databaseName}
+            """)
+
+    if recreate:
+        spark.sql(f"""
+                  DROP TABLE IF EXISTS {tableName_uc}
+                  """)
+    
+    if not spark.catalog.tableExists(tableName_uc):
+        spark.sql(f"""
+        Create table {tableName_uc}
+        using DELTA
+        Location 'abfss://bronze@adls0ig0{ENVIRONMENT}0westeurope.dfs.core.windows.net/{rootPath}/{databaseName}/{schemaName}/{tableName}/'
+        """)
+
+# COMMAND ----------
+
+for databaseName in dbutils.fs.ls(f'mnt/bronze/nuaz-sqlserver-01'):
+    for schemaName in dbutils.fs.ls(f'mnt/bronze/nuaz-sqlserver-01/{databaseName.name[:-1]}'):
+        for tableName in dbutils.fs.ls(f'mnt/bronze/nuaz-sqlserver-01/{databaseName.name[:-1]}/{schemaName.name[:-1]}'):
+            print(f"STARTING: Registering table '{tableName.name[:-1]}' to catalog 'bronze_{ENVIRONMENT}' in schema '{databaseName.name[:-1]}' .")
+
+            registerTable(rootPath = ROOT_PATH, tableName = tableName.name[:-1],schemaName=schemaName.name[:-1],databaseName=databaseName.name[:-1], recreate=REREGISTER)
+
+            print(f"FINISHED: Registering table '{tableName.name[:-1]}' to catalog 'bronze_{ENVIRONMENT}' in schema '{databaseName.name[:-1]}' .")
