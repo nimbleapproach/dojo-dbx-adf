1ec95ef1c6605f4d107c85e7a9812a502e62325f
Daniel Pludowski <Daniel.Pludowski@infinigate.com>
2024-07-16 11:56:07 +0000

    Merged PR 4800: T18486 - Dq Library
    Initial commit

--- databricks/bronze/nb-igsql01-bronze_dq.py
+++ databricks/bronze/nb-igsql01-bronze_dq.py
@@ -1,10 +1,14 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC ##### Notebook to produce Basic DQ metrics on a list of NUVIAS CRM tables
+# MAGIC ##### Notebook to produce Basic DQ metrics on a list of igsql01 tables
 
 # COMMAND ----------
 
-# MAGIC %run ../library/nb-dq-bronze-basic
+# MAGIC %run ../library/nb-enable-imports
+
+# COMMAND ----------
+
+from library.dq_bronze_basic import dq_load_bronze_stats
 
 # COMMAND ----------
 
@@ -45,76 +49,94 @@ else:
 
 # COMMAND ----------
 
-
+from collections import namedtuple
+DqConf = namedtuple("DqConf", ["full_table_name", "key_column", "attribute_columns"])
 
 # COMMAND ----------
 
-# Define the source tables you wish to perform Basic DQ Chesk againsts
-data = [
-    ("igsql01.accountbase", "AccountId", ["OwningBusinessUnit","CreatedOn","ModifiedOn","AccountNumber","StatusCode","Name","inf_businessrel_VENDOR","Inf_LastModifiedOn","Inf_CountryId","inf_businessrel_NPP_COR","inf_CurrencyCode","inf_businessrel_COMP","inf_businessrel_NPP","inf_businessrel_PP","inf_customerno","inf_invoicedispatchtype","inf_businessrelationinfo","inf_businessrel_CUSTOMER","inf_Name2","inf_businessrel_endcust","inf_businessrel_SUPPLIER"]
-),
-    ("igsql01.appointmentbase", "ActivityId", ["inf_audience","inf_AppointmentId","inf_followup_appointmentid","inf_TypeOfAppointment","inf_noofattendees","inf_appointment","Inf_ActiveIdentity","Inf_AppointmentType","inf_activitytypes","inf_Interest","pm_DatabaseName"]
-),
-    ("igsql01.businessunitbase", "BusinessUnitId", ["Name","ModifiedOn","CreatedOn"]
-),
-    ("igsql01.campaignbase", "CampaignId", ["StageId","CodeName","ExpectedResponse","TotalCampaignActivityActualCost_Base","ExpectedRevenue_Base","IsTemplate","ActualStart","ActualEnd","StatusCode","CreatedBy","OwningBusinessUnit","TotalActualCost","TypeCode","ProposedEnd","PriceListId","ExpectedRevenue","Objective","Description","BudgetedCost","PromotionCodeName","TotalCampaignActivityActualCost","ExchangeRate","OtherCost","ProcessId","pm_DatabaseName","ModifiedBy","OwnerId","Name"]
-),
-    ("igsql01.contactbase", "ContactId", ["JobTitle","LastName","CustomerTypeCode","FirstName","MiddleName","NickName","StatusCode","FullName","ModifiedOn","Telephone1","StateCode","ParentCustomerId","ParentCustomerIdType","Inf_CountryId","Description","MasterId","ParentCustomerIdName","ProcessId","CreatedOnBehalfBy","GovernmentId","AnnualIncome","OnHoldTime","SLAId","pm_DatabaseName"]
-),
-    ("igsql01.inf_countrybase", "Inf_countryId", ["CreatedOn","Inf_Description","Inf_name","ModifiedOn"]
-),
-    ("igsql01.inf_interestbase", "Inf_interestId",["CreatedOn","UTCConversionTimeZoneCode","Inf_name","Inf_Portfolio","inf_VendorDimension","statecode","pm_DatabaseName","TimeZoneRuleVersionNumber","ModifiedOn","ImportSequenceNumber","OverriddenCreatedOn","ModifiedOnBehalfBy","Inf_InterestShortcut","inf_ParentInterest","ModifiedBy","statuscode","OrganizationId","VersionNumber","CreatedOnBehalfBy","CreatedBy"]
-),
-    ("igsql01.inf_interestlinkbase", "CustomerNumber",["CustomerName"]
-),
-    ("igsql01.opportunitybase", "OpportunityId", ["ModifiedOn","CustomerIdType","CreatedOn","CustomerIdName","ParentAccountId","CustomerId","New_BusinessCategory","New_ResellerAccountId"]
-),
-    ("igsql01.ownerbase", "OwnerId", ["YomiName","OwnerIdType","Name","pm_DatabaseName","VersionNumber"]
-),
-    ("igsql01.systemuser", "VersionNumber", ["ModifiedByName","EntityImage","Address1_City","OrganizationIdName","Address1_Line2","Address2_Line3","Address1_Line3","Address1_PostalCode","Address2_Country","EmployeeId","Skills","pm_DatabaseName","Address1_PostOfficeBox","Address1_Telephone2","Address1_Telephone3","Address2_County","Address2_Latitude","Address2_Line1","Address2_StateOrProvince","Address2_Telephone1","Address2_Telephone3","MobileAlertEMail","Address2_City","Address2_Longitude","Address2_ShippingMethodCode","NickName","PersonalEMailAddress","Address2_Name","Title","PreferredEmailCode","Address1_Name","Address2_PostalCode","JobTitle","Address2_AddressId","FirstName","MiddleName","MobilePhone","Address1_Line1","Address1_ShippingMethodCode","Address2_AddressTypeCode","Address2_Telephone2","LastName","ModifiedBy","SiteId"]
-)
+# Define the source tables you wish to perform Basic DQ Check againsts
+dq_checks = [
+    DqConf(full_table_name="igsql01.accountbase", 
+           key_column="AccountId", 
+           attribute_columns=["OwningBusinessUnit","CreatedOn","ModifiedOn","AccountNumber","StatusCode",
+                              "Name","inf_businessrel_VENDOR","Inf_LastModifiedOn","Inf_CountryId",
+                              "inf_businessrel_NPP_COR", "inf_CurrencyCode","inf_businessrel_COMP",
+                              "inf_businessrel_NPP","inf_businessrel_PP","inf_customerno",
+                              "inf_invoicedispatchtype","inf_businessrelationinfo","inf_businessrel_CUSTOMER",
+                              "inf_Name2", "inf_businessrel_endcust","inf_businessrel_SUPPLIER"]),
+    DqConf(full_table_name="igsql01.appointmentbase", 
+           key_column="ActivityId", 
+           attribute_columns=["inf_audience","inf_AppointmentId","inf_followup_appointmentid","inf_TypeOfAppointment",
+                              "inf_noofattendees","inf_appointment","Inf_ActiveIdentity","Inf_AppointmentType",
+                              "inf_activitytypes","inf_Interest","pm_DatabaseName"]),
+    DqConf(full_table_name="igsql01.businessunitbase", 
+           key_column="BusinessUnitId", 
+           attribute_columns=["Name","ModifiedOn","CreatedOn"]),
+    DqConf(full_table_name="igsql01.campaignbase", 
+           key_column="CampaignId", 
+           attribute_columns=["StageId","CodeName","ExpectedResponse","TotalCampaignActivityActualCost_Base",
+                              "ExpectedRevenue_Base","IsTemplate","ActualStart","ActualEnd","StatusCode","CreatedBy",
+                              "OwningBusinessUnit","TotalActualCost","TypeCode","ProposedEnd","PriceListId",
+                              "ExpectedRevenue","Objective","Description","BudgetedCost","PromotionCodeName",
+                              "TotalCampaignActivityActualCost","ExchangeRate","OtherCost","ProcessId",
+                              "pm_DatabaseName","ModifiedBy","OwnerId","Name"]),
+    DqConf(full_table_name="igsql01.contactbase", 
+           key_column="ContactId", 
+           attribute_columns=["JobTitle","LastName","CustomerTypeCode","FirstName","MiddleName","NickName","StatusCode",
+                              "FullName","ModifiedOn","Telephone1","StateCode","ParentCustomerId","ParentCustomerIdType",
+                              "Inf_CountryId","Description","MasterId","ParentCustomerIdName","ProcessId","CreatedOnBehalfBy",
+                              "GovernmentId","AnnualIncome","OnHoldTime","SLAId","pm_DatabaseName"]),
+    DqConf(full_table_name="igsql01.inf_countrybase", 
+           key_column="Inf_countryId", 
+           attribute_columns=["CreatedOn","Inf_Description","Inf_name","ModifiedOn"]),
+    DqConf(full_table_name="igsql01.inf_interestbase", 
+           key_column="Inf_interestId", 
+           attribute_columns=["CreatedOn","UTCConversionTimeZoneCode","Inf_name","Inf_Portfolio","inf_VendorDimension",
+                              "statecode","pm_DatabaseName","TimeZoneRuleVersionNumber","ModifiedOn","ImportSequenceNumber",
+                              "OverriddenCreatedOn","ModifiedOnBehalfBy","Inf_InterestShortcut","inf_ParentInterest","ModifiedBy",
+                              "statuscode","OrganizationId","VersionNumber","CreatedOnBehalfBy","CreatedBy"]),
+    DqConf(full_table_name="igsql01.inf_interestlinkbase", 
+           key_column="CustomerNumber", 
+           attribute_columns=["CustomerName"]),
+    DqConf(full_table_name="igsql01.opportunitybase", 
+           key_column="OpportunityId", 
+           attribute_columns=["ModifiedOn","CustomerIdType","CreatedOn","CustomerIdName","ParentAccountId","CustomerId",
+                              "New_BusinessCategory","New_ResellerAccountId"]),
+    DqConf(full_table_name="igsql01.ownerbase", 
+           key_column="OwnerId", 
+           attribute_columns=["YomiName","OwnerIdType","Name","pm_DatabaseName","VersionNumber"]),
+    DqConf(full_table_name="igsql01.systemuser", 
+           key_column="SystemUserId", 
+           attribute_columns=[
+               "ModifiedByName","EntityImage","Address1_City","OrganizationIdName","Address1_Line2","Address2_Line3",
+                              "Address1_Line3","Address1_PostalCode","Address2_Country","EmployeeId", "pm_DatabaseName",
+                              "Address1_PostOfficeBox","Address1_Telephone2","Address1_Telephone3","Address2_County","Address2_Latitude",
+                              "Address2_Line1","Address2_StateOrProvince","Address2_Telephone1","Address2_Telephone3","MobileAlertEMail",
+                              "Address2_City","Address2_Longitude","Address2_ShippingMethodCode","NickName","PersonalEMailAddress",
+                              "Address2_Name","Title","PreferredEmailCode","Address1_Name","Address2_PostalCode","JobTitle", "Skill",
+                              "Address2_AddressId","FirstName","MiddleName","MobilePhone","Address1_Line1","Address1_ShippingMethodCode",
+                              "Address2_AddressTypeCode","Address2_Telephone2","LastName","ModifiedBy","SiteId"
+                              ]),
 ]
 
-
-# Create a DataFrame
-df_dq_list = spark.createDataFrame(
-    data, ["full_table_name", "key_column", "attribute_columns"]
-)
-
 # COMMAND ----------
-
-
-display(df_dq_list)
-
-# COMMAND ----------
-
-
 
 TARGET_TABLE_NAME = "bronze_dq"
 TARGET_TABLE_SCHEMA = "dq"
 TARGET = f"silver_{ENVIRONMENT}.{TARGET_TABLE_SCHEMA}.{TARGET_TABLE_NAME}"
 RECREATE = True
-RECREATE_ALL = False
 
-if RECREATE_ALL:
-    # Verify that the table exists
-    if spark.catalog.tableExists(TARGET):
-        for row in df_dq_list.select("full_table_name").distinct().collect():
-            # remove exists stats held within the bronze_dq table for the tables that are about to be processed
-            table_name = row["full_table_name"]
-            spark.sql(
-                f"delete from {TARGET} where COALESCE(schema ,'.',table) ='{table_name}'"
-            )
+res = None
 
-
-for row in df_dq_list.collect():
-    # create paramaters that are being passed into the dq_load_bronze_stats function
-    table_name = row["full_table_name"]
-    key = row["key_column"]
-    attributes = row["attribute_columns"]
-    print(f"dq_load_bronze_stats... {table_name} key {key} attributes {attributes}")
+for check in dq_checks:
+    print(f"\ndq_load_bronze_stats... \n\ttable: {check.full_table_name} \n\tkey: {check.key_column} \n\tattributes: {check.attribute_columns}")
     # execute the function to load the stats for the source table
-    dq_load_bronze_stats(src_full_table_name=table_name, column_id=key, column_attr=attributes, recreate=RECREATE)
+    res = dq_load_bronze_stats(spark,
+                                src_full_table_name=check.full_table_name,
+                                column_id=check.key_column,
+                                column_attr=check.attribute_columns,
+                                recreate=RECREATE)
+    print(res)
 
 # COMMAND ----------
 
@@ -133,5 +155,3 @@ if df_cleaned_dupes.count()>1:
 # drop if dupes
     df_cleaned = df.withColumn("row_number", F.row_number().over(window_spec)).filter(F.col("row_number") == 1).drop("row_number")
     df_cleaned.write.mode("overwrite").saveAsTable("silver_dev.dq.bronze_dq")
-
-
