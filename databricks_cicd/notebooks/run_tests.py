9f4a3b78ee62a2af39cedb01ea246780ccbde47b
Akhtar Miah <Akhtar.Miah@infinigate.com>
2024-10-07 15:12:44 +0000

    Merged PR 5332: test_unit ignored in notebook. this command overrides the pytest.ini command
    test_unit ignored in notebook. this command overrides the pytest.ini command
    
    Related work items: #22377

--- databricks_cicd/notebooks/run_tests.py
+++ databricks_cicd/notebooks/run_tests.py
@@ -18,7 +18,7 @@ print(os.getcwd())
 # Skip writing pyc files on a readonly filesystem.
 sys.dont_write_bytecode = True
 # Run pytest.
-retcode = pytest.main([".", "-v", "-p", "no:cacheprovider", "-W", "ignore::DeprecationWarning"])
+retcode = pytest.main([".", "-v", "-p", "no:cacheprovider", "-W", "ignore::DeprecationWarning", "--ignore=notebooks/test_unit"])
 
 # # Fail the cell execution if there are any test failures.
 assert retcode == 0, "The pytest invocation failed. See the log for details."
