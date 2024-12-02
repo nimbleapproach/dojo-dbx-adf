bf089d52b3c9b094e17a0512b92ded49abc677e3
Richard Louden <Richard.Louden@infinigate.com>
2024-12-02 10:52:45 +0000

    Merged PR 5715: added notebooks and jobs for PBI metrics data pull. Also updated api utils no...
    added notebooks and jobs for PBI metrics data pull. Also updated api utils notebook to be more generalised across PBI APIs
    
    Related work items: #23814

---
+++ databricks/powerbi/nb-powerbi-api-utils.py
@@ -0,0 +1,84 @@
+# Databricks notebook source
+import requests
+import base64
+import json
+
+# COMMAND ----------
+
+class InfinigateAuth(requests.auth.AuthBase):
+    def __init__(self, client_id : str, client_secret : str):
+        base_url = "https://login.microsoftonline.com/375313bf-8b9b-44af-97cd-fc2e258a968e"
+        get_token_url =  f"{base_url}/oauth2/token"
+        grantType = 'client_credentials'
+        resource = "https://analysis.windows.net/powerbi/api"
+        
+        request_body = json.loads(json.dumps({"client_id" : client_id,
+                                   "client_secret": client_secret ,
+                                   "grant_type" : grantType,
+                                   "resource" : "https://analysis.windows.net/powerbi/api"}, indent=2))
+        
+        
+        response = requests.post(get_token_url
+              , data = request_body,
+                               headers={
+                                   'Content-Type':'application/x-www-form-urlencoded'
+                               }
+                              )
+        result = response.json()["access_token"]
+        self.authorization = f"Bearer {result}"
+        
+    def __call__(self, r):
+        # modify and return the request
+        r.headers['Authorization'] = self.authorization
+        r.headers['Content-Type'] = "application/json"
+        return r
+
+# COMMAND ----------
+
+class PowerBIHandler():
+    def __init__(self, session : requests.Session, client_id : str, client_secret : str):
+        self.baseURL = "https://api.powerbi.com/v1.0/myorg/"
+        self.session = session
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def getDailyReportViews(self, intial_run : bool, date: str, continuation_uri: str):
+        if intial_run == True:
+            metaData = self.session.get(f"{self.baseURL}admin/activityevents?startDateTime='{date}T00:00:00.000Z'&endDateTime='{date}T23:59:59.000Z'&$filter=Activity eq 'viewreport'" , auth=InfinigateAuth(self.client_id, self.client_secret))
+        else:
+            metaData = self.session.get(continuation_uri , auth=InfinigateAuth(self.client_id, self.client_secret))
+        return metaData.json()
+
+    def getRefreshDataset(self, datasetId : int , workspaceId : str):
+        metaData = self.session.get(f'{self.baseURL}groups/{workspaceId}/datasets/{datasetId}/refreshes' , auth=InfinigateAuth(self.client_id, self.client_secret))
+        return metaData.json()
+    
+    def getLatestDatasetAPIRefreshStatus(self, datasetId : int , workspaceId : str):
+        metaData = self.session.get(f'{self.baseURL}groups/{workspaceId}/datasets/{datasetId}/refreshes' , auth=InfinigateAuth(self.client_id, self.client_secret))
+        response = metaData.json()
+        refreshHistory = response.get("value")
+        return sorted([refresh for refresh in refreshHistory if refresh['refreshType'] == 'ViaApi'], key=lambda x: x['startTime'],reverse=True)[0]["status"]
+    
+    def postRefreshDataset(self, datasetId : int , workspaceId : str):
+        print(f'BEGIN: Refreshing dataset {datasetId} in workspace {workspaceId}.')
+        self.session.post(f'{self.baseURL}groups/{workspaceId}/datasets/{datasetId}/refreshes' , auth=InfinigateAuth(self.client_id, self.client_secret))
+
+    def getDataflowTransactions(self, dataflowId : int , workspaceId : str):
+        metaData = self.session.get(f'{self.baseURL}groups/{workspaceId}/dataflows/{dataflowId}/transactions' , auth=InfinigateAuth(self.client_id, self.client_secret))
+        return metaData.json()
+
+    def postRefreshDataflow(self, dataflowId : int , workspaceId : str):
+        print(f'BEGIN: Refreshing dataflow {dataflowId} in workspace {workspaceId}.')
+        request_body = json.dumps(
+            {"refreshRequest": 'y'})
+        
+        requestHeader = {}
+        requestHeader["Content-Type"] = "application/json"
+                                  
+        return self.session.post(f'{self.baseURL}groups/{workspaceId}/dataflows/{dataflowId}/refreshes', headers=requestHeader,data=request_body, auth=InfinigateAuth(self.client_id, self.client_secret))
+    
+    def getLatestDataflowAPIRefreshStatus(self, dataflowId : int , workspaceId : str):
+        metaData = self.session.get(f'{self.baseURL}groups/{workspaceId}/dataflows/{dataflowId}/transactions' , auth=InfinigateAuth(self.client_id, self.client_secret))
+        response = metaData.json()
+        refreshHistory = response.get("value")
+        return sorted([refresh for refresh in refreshHistory if refresh['refreshType'] == 'ViaApi'], key=lambda x: x['startTime'],reverse=True)[0]["status"]
---
+++ databricks_cicd/notebooks/powerbi/nb-powerbi-metrics-pull.py
@@ -0,0 +1,45 @@
+# Databricks notebook source
+import os
+from datetime import datetime, timedelta
+
+ENVIRONMENT = os.environ["__ENVIRONMENT__"]
+
+# COMMAND ----------
+
+# MAGIC %run  ./nb-powerbi-api-utils
+
+# COMMAND ----------
+
+client_id = dbutils.secrets.get(scope="kv-ig-westeurope-shared",key="secret-powerbi-admin-app-id")
+client_secret = dbutils.secrets.get(scope="kv-ig-westeurope-shared",key="secret-powerbi-admin-app-secret")
+metrics_date = (datetime.now() - timedelta(days = 1)).strftime("%Y-%m-%d")
+infinigateSession = requests.Session()
+
+# COMMAND ----------
+
+PowerBIHandler = PowerBIHandler(infinigateSession, client_id, client_secret)
+
+# COMMAND ----------
+
+activity_list = []
+
+initial_call = PowerBIHandler.getDailyReportViews(intial_run = True, date = metrics_date, continuation_uri = "NA")
+initial_activities = initial_call.get("activityEventEntities")
+activity_list = activity_list + initial_activities
+continuation_uri = initial_call.get("continuationUri")
+
+while continuation_uri:
+    ## if continuation uri present, get data
+    call = PowerBIHandler.getDailyReportViews(intial_run = False, date = "NA", continuation_uri = continuation_uri)
+    activities = call.get("activityEventEntities")
+    activity_list = activity_list + activities
+    continuation_uri = call.get("continuationUri")
+
+# COMMAND ----------
+
+if len(activity_list) != 0:
+    activity_df = spark.createDataFrame(activity_list)
+    (activity_df.write.format("delta")
+        .option("mergeSchema", "true")
+        .mode("append")
+        .saveAsTable(f"bronze_{ENVIRONMENT}.powerbi.activities"))
