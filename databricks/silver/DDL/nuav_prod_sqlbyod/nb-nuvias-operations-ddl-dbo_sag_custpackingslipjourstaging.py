ea9409c0000003850dea273c83f06ae937776634
Yehor Holyk <Yehor.Holyk@infinigate.com>
2024-12-08 08:55:48 +0000

    Merged PR 5820: 23870 Added tables/attributes for CSC pos report
    23870 Added tables/attributes for CSC pos report
    
    Related work items: #23870

--- databricks/silver/DDL/nuav_prod_sqlbyod/nb-nuvias-operations-ddl-dbo_sag_custpackingslipjourstaging.py
+++ databricks/silver/DDL/nuav_prod_sqlbyod/nb-nuvias-operations-ddl-dbo_sag_custpackingslipjourstaging.py
@@ -16,17 +16,13 @@ spark.catalog.setCurrentCatalog(f"silver_{ENVIRONMENT}")
 
 # MAGIC %sql
 # MAGIC
-# MAGIC CREATE OR REPLACE TABLE dbo_sag_inventdimstaging
+# MAGIC CREATE OR REPLACE TABLE dbo_sag_custpackingslipjourstaging
 # MAGIC   (
 # MAGIC     SID BIGINT GENERATED ALWAYS AS IDENTITY
 # MAGIC       COMMENT 'Surrogate Key'
-# MAGIC   , INVENTDIMID STRING
+# MAGIC   , RECID BIGINT
 # MAGIC       COMMENT 'TODO'
-# MAGIC   , DATAAREAID STRING
-# MAGIC       COMMENT 'TODO'
-# MAGIC   , INVENTLOCATIONID STRING
-# MAGIC       COMMENT 'TODO'
-# MAGIC   , WMSLOCATIONID STRING
+# MAGIC   , PACKINGSLIPID STRING
 # MAGIC       COMMENT 'TODO'
 # MAGIC   , Sys_Bronze_InsertDateTime_UTC TIMESTAMP
 # MAGIC       COMMENT 'The timestamp when this entry landed in bronze.'
@@ -39,21 +35,21 @@ spark.catalog.setCurrentCatalog(f"silver_{ENVIRONMENT}")
 # MAGIC   , Sys_Silver_HashKey BIGINT NOT NULL
 # MAGIC       COMMENT 'HashKey over all but Sys columns.'
 # MAGIC   , Sys_Silver_IsCurrent BOOLEAN
-# MAGIC   , CONSTRAINT dbo_sag_inventdimstaging_pk PRIMARY KEY(INVENTDIMID, DATAAREAID, Sys_Bronze_InsertDateTime_UTC)
+# MAGIC   , CONSTRAINT dbo_sag_custpackingslipjourstaging_pk PRIMARY KEY(RECID, Sys_Bronze_InsertDateTime_UTC)
 # MAGIC   )
-# MAGIC COMMENT 'This table contains the data from dbo_sag_inventdimstaging. \n'
+# MAGIC COMMENT 'This table contains the data from dbo_sag_custpackingslipjourstaging. \n'
 # MAGIC TBLPROPERTIES ('delta.feature.allowColumnDefaults' = 'supported');
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC
-# MAGIC ALTER TABLE dbo_sag_inventdimstaging ADD CONSTRAINT dateWithinRange_Bronze_InsertDateTime CHECK (Sys_Bronze_InsertDateTime_UTC > '1900-01-01');
-# MAGIC ALTER TABLE dbo_sag_inventdimstaging ADD CONSTRAINT dateWithinRange_Silver_InsertDateTime CHECK (Sys_Silver_InsertDateTime_UTC > '1900-01-01');
-# MAGIC ALTER TABLE dbo_sag_inventdimstaging ADD CONSTRAINT dateWithinRange_Silver_ModifedDateTime CHECK (Sys_Silver_ModifedDateTime_UTC > '1900-01-01');
+# MAGIC ALTER TABLE dbo_sag_custpackingslipjourstaging ADD CONSTRAINT dateWithinRange_Bronze_InsertDateTime CHECK (Sys_Bronze_InsertDateTime_UTC > '1900-01-01');
+# MAGIC ALTER TABLE dbo_sag_custpackingslipjourstaging ADD CONSTRAINT dateWithinRange_Silver_InsertDateTime CHECK (Sys_Silver_InsertDateTime_UTC > '1900-01-01');
+# MAGIC ALTER TABLE dbo_sag_custpackingslipjourstaging ADD CONSTRAINT dateWithinRange_Silver_ModifedDateTime CHECK (Sys_Silver_ModifedDateTime_UTC > '1900-01-01');
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC
-# MAGIC ALTER TABLE silver_dev.nuav_prod_sqlbyod.dbo_sag_inventdimstaging OWNER TO `az_edw_data_engineers_ext_db`
+# MAGIC ALTER TABLE silver_dev.nuav_prod_sqlbyod.dbo_sag_custpackingslipjourstaging OWNER TO `az_edw_data_engineers_ext_db`
